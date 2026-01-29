59dbdb921107fee882b41a4552a7e19457801908
Diala wilson <diala.i.wilson@gmail.com>
2026-01-29 17:50:03 +0100

    feat: Add new Dart examples for comments and data types, update the main executable to run data type examples, and remove an old test.

---
+++ lib/class_2_comments.dart
@@ -0,0 +1,13 @@
+void main(){
+  // This is a single-line comment
+  print("Hello world");
+
+  /* This is a multi-line comment
+    
+  It can span multiple lines
+  */
+
+  /// This is a documentation comment
+  /// It is used to document the code
+  /// It can be used to generate documentation
+}
