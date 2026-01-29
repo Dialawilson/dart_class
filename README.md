bd551e721830f5eac61b627fe2e8645bd52705c1
Diala wilson <diala.i.wilson@gmail.com>
2026-01-29 14:24:19 +0100

    introduction

---
+++ README.md
@@ -0,0 +1,41 @@
+🛠 Option 1: Using VS Code (Recommended)
+This is the easiest way to start if you already have VS Code installed.
+
+Open VS Code.
+
+Press Ctrl + Shift + P (or Cmd + Shift + P on Mac) to open the Command Palette.
+
+Type and select "Dart: New Project".
+
+Select "Console Application".
+
+Choose the folder where you want to save your project.
+
+Enter your Project Name.
+
+Click "Create". Your new Dart project folder will open automatically.
+
+=================
+                             OR 
+==================
+
+⚙️ Option 2: Manual SDK Installation
+Use this method if you want to run Dart from your terminal or command prompt.
+
+Download: Get the SDK from dart.dev/get-dart.
+
+Extract: Unzip the downloaded folder.
+
+Install: Copy the extracted SDK folder and paste it into your C:\ drive.
+
+Copy Path: Open the Dart folder in your C:\ drive, go into the bin folder, and copy the full file path from the top address bar.
+
+Environment Variables: * Search for "Environment Variables" in Windows.
+
+Under "System Variables," find Path, click Edit, then click New.
+
+Paste the path to the bin folder.
+
+Verify: Open your terminal and type: dart --version to check if it is installed 
+
+
---
+++ lib/class_1_hello_word.dart
@@ -0,0 +1,22 @@
+void main(){
+  print("Hello world");
+}
+
+
+// void (The Return Type)
+// This keyword tells Dart that this function does not return any value. It performs an action (printing text) and then finishes. If the function was going to give back a number, you would use int instead of void.
+
+// 2. main() (The Function Name)
+// This is a special reserved name. When you run your program, the Dart Virtual Machine looks specifically for a function named main.
+
+// The parentheses () are used to hold parameters (inputs), though in this simple version, they are empty.
+
+// 3. { ... } (The Code Block)
+// The curly braces define the "body" of the function. Everything inside these braces is what the computer will actually do when the function is called.
+
+// 4. print("Hello world"); (The Instruction)
+// print: This is a built-in function that tells the computer to display whatever is inside the parentheses to the console (terminal).
+
+// "Hello world": This is a String (text). In Dart, Strings must be wrapped in either single quotes ' or double quotes ".
+
+// ; (The Semicolon): Think of this as a "period" at the end of a sentence. In Dart, every standalone instruction must end with a semicolon to tell the compiler the statement is finished.
