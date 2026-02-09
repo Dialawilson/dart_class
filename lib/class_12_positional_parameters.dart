65a4afe192129a3e9d146ae930d42c207d61fd6c
Diala wilson <diala.i.wilson@gmail.com>
2026-02-09 08:55:54 +0100

    class_12_positional_parameters

--- lib/class_12_positional_parameters.dart
+++ lib/class_12_positional_parameters.dart
@@ -6,9 +6,20 @@ void main(){
 
   cities("New York", "Los Angeles", "Chicago");
   // if you miss a particular parameter it will throw an error
+
+  places("New York",  );
 } 
 
+// REQUIRED PARAMETER
 void cities(String city1, String city2, String city3){
+  print("The first city is $city1");
+  print("The second city is $city2");
+  print("The third city is $city3");
+}
+
+// OPTIONAL PARAMETER
+// it allows you to pass a parameter to a function without a value
+void places(String city1, [ city2,  city3]){
   print("The first city is $city1");
   print("The second city is $city2");
   print("The third city is $city3");
